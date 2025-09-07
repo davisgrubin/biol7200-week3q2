b170285911cc41b009563fc6266fcce78bfd4a89
Davis Grubin <davisgrubin@gmail.com>
2025-09-07 15:19:21 -0400

    change >> to > and print wc -l to stdout

--- find_homologs.sh
+++ find_homologs.sh
@@ -3,4 +3,4 @@ query_file="$1"
 subject_file="$2"
 output_file="$3"
 
-tblastn -query "$query_file" -subject "$subject_file" -outfmt "6 qseqid sseqid pident length qlen" | awk -v output_file="$output_file" '$3 > 30 && ($4 / $5) * 100 > 90 { print $0 >> output_file; count++ } END { print count }'
+tblastn -query "$query_file" -subject "$subject_file" -outfmt "6 qseqid sseqid pident length qlen" | awk '$3 > 30 && ($4 / $5) * 100 > 90 { print $0 }' > "$output_file" && wc -l < "$output_file"
